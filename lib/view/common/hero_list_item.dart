62b4a677cc27d5bb3897bce691978d509a9b45b8
Arran France <arranf@gmail.com>
2017-12-11 10:43:55 +0000

    Style HeroListItem

--- lib/view/common/hero_list_item.dart
+++ lib/view/common/hero_list_item.dart
@@ -14,9 +14,27 @@ class HeroListItem extends StatelessWidget{
 
   @override
    Widget build(BuildContext context) {
-     return new Row(key: new Key(heroInfo.name + '_hero_info'), children: <Widget>[
-       new Text(heroInfo.name),
-       new Text(heroInfo.role), 
-     ],);
+     return new Container(
+       margin: new EdgeInsets.all(8.0),
+       child: new Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+           new Container(height: 2.0,),
+           new Text(
+             heroInfo.name,
+            style: new TextStyle(
+              fontSize: 18.0
+            ),),
+            new Container(height: 8.0,),
+            new Text(heroInfo.role),
+            new Container(
+              margin: new EdgeInsets.symmetric(vertical: 8.0),
+              height: 2.0,
+              width: 18.0,
+              color: Colors.deepPurpleAccent
+          ),
+         ],
+       ),
+     );
    }
 }
