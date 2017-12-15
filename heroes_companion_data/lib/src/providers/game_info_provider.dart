676364e476b5e126f0557b25d45ddf2c4bfb0247
Arran France <arranf@gmail.com>
2017-12-15 10:05:10 +0000

    Add exceptions on data fetching fail

--- heroes_companion_data/lib/src/providers/game_info_provider.dart
+++ heroes_companion_data/lib/src/providers/game_info_provider.dart
@@ -2,12 +2,19 @@ import 'dart:async';
 import 'package:hots_dog_api/hots_dog_api.dart' as api;
 
 class GameInfoProvider {
-  Future<api.GameInfo> getGameInfo() {
-    return api.getGameInfo();
+  Future<api.GameInfo> getGameInfo() async {
+    api.GameInfo gameInfo = await api.getGameInfo();
+    if (gameInfo == null) {
+      throw new Exception('API call to fetch builds failed');
+    }
+    return gameInfo;
   }
 
   Future<List<api.BuildInfo>> getBuilds() async {
-    api.GameInfo gameinfo = await getGameInfo();
-    return gameinfo.buildInfo;
+    api.GameInfo gameInfo = await getGameInfo();
+    if (gameInfo == null) {
+      throw new Exception('API call to fetch builds failed');
+    }
+    return gameInfo.buildInfo;
   }
 }
--- heroes_companion_data/lib/src/providers/win_rate_provider.dart
+++ heroes_companion_data/lib/src/providers/win_rate_provider.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
 import 'package:hots_dog_api/hots_dog_api.dart' as api;
+import 'package:hots_dog_api/hots_dog_api.dart';
 
 class WinRateProvider {
-  Future<api.WinRates> getWinRates(String buildNumber) {
-    return api.getWinRates(buildNumber);
+  Future<api.WinRates> getWinRates(String buildNumber) async {
+    WinRates winRates = await api.getWinRates(buildNumber);
+    if (winRates == null) {
+      throw new Exception('API call to fetch builds failed');
+    }
+    return winRates;
   }
 }
