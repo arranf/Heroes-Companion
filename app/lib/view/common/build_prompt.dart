ce2d5dc011c6ff7eb5d9b7331dd788e008db26e4
Arran France <arranf@gmail.com>
2017-12-22 08:56:45 +0000

    Use addPostFrameCallback rather than a future

--- app/lib/view/common/build_prompt.dart
+++ app/lib/view/common/build_prompt.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/material.dart';
 import 'package:hots_dog_api/hots_dog_api.dart';
 
@@ -16,44 +17,39 @@ class BuildPrompt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Builder(
-      builder: (BuildContext context){
-        if (winLossCount != null && !_isCurrentBuild) {
-          return new GestureDetector(
-              onTap: onTap,
-              child: new Container(
-                  constraints: new BoxConstraints.expand(height: 36.0),
-                  color: _isCurrentBuild
-                      ? Theme.of(context).errorColor
-                      : Theme.of(context).accentColor,
-                  child: new Center(
-                      child: new Text(
-                        'Showing previous build data'.toUpperCase(),
-                      style: Theme.of(context).textTheme.body1.copyWith(
-                        fontWeight: FontWeight.w600,
-                      )
-                      .apply(fontSizeFactor: 0.9),
-                  ))));
-        } else if (winLossCount != null && _isCurrentBuild && (winLossCount.losses + winLossCount.wins < 1300)){
-          new Future<Null>.delayed(new Duration(seconds: 1), () {
-            Scaffold.of(context).showSnackBar(
-              new SnackBar(
-                content: new Text('Not many games have been played this patch. Would you like to see the builds for the previous patch?'),
-                duration: new Duration(seconds: 10),
-                action: new SnackBarAction(
-                  onPressed: onTap,
-                  label: 'Show Me',
-                ),
-              )
-            );
-          });
-          
-          
-        }
-        return new Container(
-          height: 0.0,
+    if (winLossCount != null && !_isCurrentBuild) {
+      return new GestureDetector(
+          onTap: onTap,
+          child: new Container(
+              constraints: new BoxConstraints.expand(height: 36.0),
+              color: _isCurrentBuild
+                  ? Theme.of(context).errorColor
+                  : Theme.of(context).accentColor,
+              child: new Center(
+                  child: new Text(
+                    'Showing previous build data'.toUpperCase(),
+                  style: Theme.of(context).textTheme.body1.copyWith(
+                    fontWeight: FontWeight.w600,
+                  )
+                  .apply(fontSizeFactor: 0.9),
+              ))));
+    } else if (winLossCount != null && _isCurrentBuild && (winLossCount.losses + winLossCount.wins < 1300)){
+      
+      SchedulerBinding.instance.addPostFrameCallback( (duration) {
+        Scaffold.of(context).showSnackBar(
+          new SnackBar(
+            content: new Text('Not many games have been played this patch. Would you like to see the builds for the previous patch?'),
+            duration: new Duration(seconds: 10),
+            action: new SnackBarAction(
+              onPressed: onTap,
+              label: 'Show Me',
+            ),
+          )
         );
-      },
+      });
+    }
+    return new Container(
+      height: 0.0,
     );
   }
 }
