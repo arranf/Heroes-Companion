a915134da1c399d2f69bba3371167843a853d40c
Arran France <arranf@gmail.com>
2017-12-28 14:19:19 +0000

    Add tabs

--- app/lib/view/common/hero_detail.dart
+++ app/lib/view/common/hero_detail.dart
@@ -7,7 +7,7 @@ import 'package:heroes_companion_data/heroes_companion_data.dart';
 import 'package:hots_dog_api/hots_dog_api.dart' hide Talent;
 import 'package:meta/meta.dart';
 
-class HeroDetail extends StatelessWidget {
+class HeroDetail extends StatefulWidget {
   final Hero hero;
   final bool isCurrentBuild;
   final WinLossCount winLossCount;
@@ -26,7 +26,27 @@ class HeroDetail extends StatelessWidget {
     this.buildNumber,
     this.buildSwitch,
   })
-    : super(key: key);
+      : super(key: key);
+
+  @override
+  State createState() => new _HeroDetailState();
+}
+
+class _HeroDetailState extends State<HeroDetail>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +57,14 @@ class HeroDetail extends StatelessWidget {
 
   Widget _buildTitleRow(BuildContext context) {
     return new Container(
-      key: new Key(hero.name + '_title_row'),
+      key: new Key(widget.hero.name + '_title_row'),
       padding: new EdgeInsets.only(left: 64.0, bottom: 8.0),
       color: Theme.of(context).primaryColor,
       child: new Row(
         children: <Widget>[
           new CircleAvatar(
-            backgroundImage:
-                new AssetImage('assets/images/heroes/${hero.icon_file_name}'),
+            backgroundImage: new AssetImage(
+                'assets/images/heroes/${widget.hero.icon_file_name}'),
             radius: 45.0,
           ),
           new Container(
@@ -53,14 +73,14 @@ class HeroDetail extends StatelessWidget {
           new Padding(
             padding: new EdgeInsets.only(top: 4.0),
             child: new Column(children: [
-              new Text(hero.name,
+              new Text(widget.hero.name,
                   style: Theme
                       .of(context)
                       .textTheme
                       .headline
                       .apply(color: Colors.white)),
               new Text(
-                '${hero.type} ${hero.role}',
+                '${widget.hero.type} ${widget.hero.role}',
                 style: Theme
                     .of(context)
                     .textTheme
@@ -71,8 +91,8 @@ class HeroDetail extends StatelessWidget {
                 height: 20.0,
               ),
               new Text(
-                winLossCount != null
-                    ? '${winLossCount.winPercentange().toStringAsFixed(1)} Win %'
+                widget.winLossCount != null
+                    ? '${widget.winLossCount.winPercentange().toStringAsFixed(1)} Win %'
                     : ' ',
                 style: Theme
                     .of(context)
@@ -81,8 +101,8 @@ class HeroDetail extends StatelessWidget {
                     .apply(color: Colors.white),
               ),
               new Text(
-                  winLossCount != null
-                      ? '${(winLossCount.wins + winLossCount.losses).toString()} games played'
+                  widget.winLossCount != null
+                      ? '${(widget.winLossCount.wins + widget.winLossCount.losses).toString()} games played'
                       : ' ',
                   style: Theme
                       .of(context)
@@ -97,58 +117,49 @@ class HeroDetail extends StatelessWidget {
   }
 
   Widget _buildTalentRows(BuildContext context) {
-    if (buildWinRates != null) {
+    if (widget.buildWinRates != null) {
       List<Widget> children = [];
-      if (buildWinRates.winning_builds != null) {
+      if (widget.buildWinRates.winning_builds != null) {
         List<BuildStatistics> interestingWinningBuilds =
-            new List<BuildStatistics>.from(buildWinRates.winning_builds
+            new List<BuildStatistics>.from(widget.buildWinRates.winning_builds
                 .where((b) => b.talents_names.length == 7));
         if (interestingWinningBuilds.length > 0) {
-          children.add(new Text(
-            'Winning Builds',
-            style: Theme.of(context).textTheme.headline,
+          children.add(new ListView(
+            children: interestingWinningBuilds
+                .map((b) => _buildTalentRow(context, b))
+                .toList(),
           ));
-          List<Widget> winningBuilds = new List.generate(
-              interestingWinningBuilds.length,
-              (i) => _buildTalentRow(context, interestingWinningBuilds[i]));
-          children.addAll(winningBuilds);
         }
       }
 
-      if (buildWinRates.popular_builds != null) {
+      if (widget.buildWinRates.popular_builds != null) {
         List<BuildStatistics> interestingPopularBuilds =
-            new List<BuildStatistics>.from(buildWinRates.popular_builds
+            new List<BuildStatistics>.from(widget.buildWinRates.popular_builds
                 .where((b) => b.talents_names.length == 7));
         if (interestingPopularBuilds.length > 0) {
-          children.add(new Text(
-            'Popular Builds',
-            style: Theme.of(context).textTheme.headline,
-          ));
-          List<Widget> popularBuilds = new List.generate(
-              interestingPopularBuilds.length,
-              (i) => _buildTalentRow(context, interestingPopularBuilds[i]));
-          children.addAll(popularBuilds);
-        } else {
-          children.add(new Text(
-            'No Popular Builds for ${hero.name}',
-            style: Theme.of(context).textTheme.headline,
+          children.add(new ListView(
+            children: interestingPopularBuilds
+                .map((b) => _buildTalentRow(context, b))
+                .toList(),
           ));
         }
       }
-
-      return new Column(key: new Key(hero.name + '_talent_rows'), children: children);
+      return new TabBarView(
+        key: new Key(widget.hero.name + '_talent_rows'),
+        children: children,
+      );
     }
     return new Container();
   }
 
   Widget _buildTalentRow(BuildContext context, BuildStatistics build) {
     return new Padding(
-        key: new Key('${hero.name}_talent_row_${build.hashCode}'),
+        key: new Key('${widget.hero.name}_talent_row_${build.hashCode}'),
         padding: new EdgeInsets.all(4.0),
         child: new Column(
           children: [
             new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 new Padding(
                   padding: new EdgeInsets.symmetric(horizontal: 4.0),
@@ -160,13 +171,20 @@ class HeroDetail extends StatelessWidget {
                   child: new Text('${build.total_games_played} Games Played'),
                 ),
                 new Padding(
-                  padding: new EdgeInsets.symmetric(horizontal: 4.0),
-                  child: new IconButton(icon: new Icon(Icons.play_circle_filled), onPressed: () {
-                    Navigator.of(context).push(new PageRouteBuilder(
-                      pageBuilder: (context, a1, a2) => new BuildSwiper(hero, build, key: new Key('${hero.name}_${build.hashCode}_build_swiper'),),
-                    ));
-                  })
-                )
+                    padding: new EdgeInsets.symmetric(horizontal: 4.0),
+                    child: new IconButton(
+                        icon: new Icon(Icons.play_circle_filled),
+                        onPressed: () {
+                          Navigator.of(context).push(new PageRouteBuilder(
+                                pageBuilder: (context, a1, a2) =>
+                                    new BuildSwiper(
+                                      widget.hero,
+                                      build,
+                                      key: new Key(
+                                          '${widget.hero.name}_${build.hashCode}_build_swiper'),
+                                    ),
+                              ));
+                        }))
               ],
             ),
             new Container(
@@ -174,12 +192,10 @@ class HeroDetail extends StatelessWidget {
             ),
             new Padding(
               padding: new EdgeInsets.symmetric(horizontal: 8.0),
-              child: new FittedBox(
-                fit: BoxFit.fitWidth,
-                child: new Row(
-                  children: new List.generate(build.talents_names.length,
-                    (i) => _buildTalent(context, build.talents_names[i])))
-              ),
+              child: new Row(
+                  children: build.talents_names
+                      .map((a) => _buildTalent(context, a))
+                      .toList()),
             ),
           ],
         ));
@@ -187,66 +203,85 @@ class HeroDetail extends StatelessWidget {
 
   Widget _buildTalent(BuildContext context, String talentName) {
     Talent talent =
-        hero.talents.firstWhere((t) => t.talent_tree_id == talentName);
-    return new GestureDetector(
-      onTap: () => showModalBottomSheet<Null>(context: context, builder: (BuildContext context) {
-              return new Container(
-                child: new Container(
-                  child: new Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: new Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        new Row(
-                          children: <Widget>[
-                            new Text(talent.name, style: Theme.of(context).textTheme.headline,),
-                            new Image.asset('assets/images/talents/${talent.icon_file_name}')
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        ),
-                        new Container(height: 16.0,),
-                        new Text(talent.description)
-                      ],
-                    )
-                  ),
-                )
-              );
-            }),
-      child: new Image.asset('assets/images/talents/${talent.icon_file_name}'),
-    );
+        widget.hero.talents.firstWhere((t) => t.talent_tree_id == talentName);
+    return new Flexible(
+        child: new GestureDetector(
+      onTap: () => showModalBottomSheet<Null>(
+          context: context,
+          builder: (BuildContext context) {
+            return new Container(
+              child: new Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Row(
+                        children: <Widget>[
+                          new Text(
+                            talent.name,
+                            style: Theme.of(context).textTheme.headline,
+                          ),
+                          new Image.asset(
+                              'assets/images/talents/${talent.icon_file_name}')
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      ),
+                      new Container(
+                        height: 16.0,
+                      ),
+                      new Text(talent.description)
+                    ],
+                  )),
+            );
+          }),
+      child: new Image.asset(
+        'assets/images/talents/${talent.icon_file_name}',
+      ),
+      // child: new Placeholder(fallbackHeight: 64.0, fallbackWidth: 64.0,),
+    ));
   }
 
   Widget _buildDetail(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text(hero.name),
+          title: new Text(widget.hero.name),
           actions: [
             new IconButton(
-              tooltip: hero.is_favorite
-                  ? 'Unfavorite ${hero.name}'
-                  : 'Favorite ${hero.name}',
+              tooltip: widget.hero.is_favorite
+                  ? 'Unfavorite ${widget.hero.name}'
+                  : 'Favorite ${widget.hero.name}',
               icon: new Icon(Icons.favorite,
-                  color: hero.is_favorite
+                  color: widget.hero.is_favorite
                       ? Colors.red
                       : Theme.of(context).buttonColor),
-              onPressed: () => this.favorite(this.hero),
+              onPressed: () => widget.favorite(widget.hero),
             ),
           ],
         ),
-        body: new Column(
-          children: <Widget>[
-            new BuildPrompt(isCurrentBuild, winLossCount, buildSwitch, key: new Key('${hero.name}_previous_build_prompt'),),
-            _buildTitleRow(context),
-            new Flexible(
-                child: new ListView(
+        body: new DefaultTabController(
+            length: 2,
+            child: new Column(
               children: <Widget>[
-                new Container(
-                  height: 24.0,
+                new BuildPrompt(
+                  widget.isCurrentBuild,
+                  widget.winLossCount,
+                  widget.buildSwitch,
+                  key: new Key('${widget.hero.name}_previous_build_prompt'),
                 ),
-                _buildTalentRows(context)
+                _buildTitleRow(context),
+                new TabBar(
+                  tabs: [
+                    new Tab(
+                      text: 'Winning',
+                    ),
+                    new Tab(text: 'Popular')
+                  ],
+                  labelColor: Theme.of(context).textTheme.body2.color,
+                ),
+                new Expanded(
+                  child: _buildTalentRows(context),
+                ),
               ],
-            ))
-          ],
-        ));
+            )));
   }
 }
