b1685f6ea9aa07de8628d82997d73b415b2c32e7
Arran France <arranf@gmail.com>
2018-02-02 21:25:42 +0000

    Simplify Hero Detail

--- app/lib/view/common/hero_detail.dart
+++ app/lib/view/common/hero_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' hide Hero;
 import 'package:heroes_companion/view/common/app_loading_container.dart';
+import 'package:heroes_companion/view/common/build_card.dart';
 import 'package:heroes_companion/view/common/build_prompt.dart';
 import 'package:heroes_companion/view/common/build_swiper.dart';
 import 'package:heroes_companion/view/common/loading_view.dart';
@@ -174,61 +175,15 @@ class HeroDetail extends StatelessWidget {
     );
   }
 
-  Widget _buildTalentCard(
-      BuildContext context, BuildStatistics build, String type) {
-    return new Container(
-      padding: new EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-      child: new Card(
-        child: new Container(
-          padding: new EdgeInsets.symmetric(horizontal: 16.0)
-              .add(new EdgeInsets.only(top: 24.0, bottom: 8.0)),
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Padding(
-                padding: new EdgeInsets.only(bottom: 16.0),
-                child: new Text(
-                    '${(build.win_rate * 100).toStringAsFixed(2)} Win %',
-                    style: new TextStyle(
-                        fontSize: 20.0, fontWeight: FontWeight.w600)),
-              ),
-              new Text('${build.total_games_played} Games Played',
-                  style: new TextStyle(fontSize: 16.0)),
-              new Container(
-                height: 16.0,
-              ),
-              new Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: build.talents_names
-                      .map((a) => _buildCardTalent(context, a))
-                      .toList()),
-              new ButtonTheme.bar(
-                child: new ButtonBar(
-                  children: <Widget>[
-                    new FlatButton(
-                      child: const Text('PLAY BUILD'),
-                      onPressed: () {
-                        Navigator.of(context).push(new PageRouteBuilder(
+  void _playBuild(BuildContext context, BuildStatistics buildStatistics) {
+    Navigator.of(context).push(new PageRouteBuilder(
                               pageBuilder: (context, a1, a2) => new BuildSwiper(
                                     hero,
-                                    build,
+                                    buildStatistics,
                                     key: new Key(
                                         '${hero.name}_${build.hashCode}_build_swiper'),
                                   ),
                             ));
-                      },
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 
   Widget _buildTalentCards(BuildContext context) {
@@ -240,7 +195,7 @@ class HeroDetail extends StatelessWidget {
                 .where((b) => b.talents_names.length == 7));
         if (interestingWinningBuilds.length > 0) {
           children.addAll(interestingWinningBuilds
-              .map((b) => _buildTalentCard(context, b, 'Winning Build'))
+              .map((BuildStatistics b) => new BuildCard(b, 'Winning Build', _playBuild, hero, showTalentBottomSheet))
               .toList());
         }
       }
@@ -251,7 +206,7 @@ class HeroDetail extends StatelessWidget {
                 .where((b) => b.talents_names.length == 7));
         if (interestingPopularBuilds.length > 0) {
           children.addAll(interestingPopularBuilds
-              .map((b) => _buildTalentCard(context, b, 'Popular Build'))
+              .map((b) => new BuildCard(b, 'Popular Build', _playBuild, hero, showTalentBottomSheet))
               .toList());
         }
       }
@@ -261,92 +216,6 @@ class HeroDetail extends StatelessWidget {
       );
     }
     return new Container();
-  }
-
-  Widget _buildTalentRows(BuildContext context) {
-    if (buildWinRates != null) {
-      List<Widget> children = [];
-      if (buildWinRates.winning_builds != null) {
-        List<BuildStatistics> interestingWinningBuilds =
-            new List<BuildStatistics>.from(buildWinRates.winning_builds
-                .where((b) => b.talents_names.length == 7));
-        if (interestingWinningBuilds.length > 0) {
-          children.add(new ListView(
-            children: interestingWinningBuilds
-                .map((b) => _buildTalentRow(context, b))
-                .toList(),
-          ));
-        }
-      }
-
-      if (buildWinRates.popular_builds != null) {
-        List<BuildStatistics> interestingPopularBuilds =
-            new List<BuildStatistics>.from(buildWinRates.popular_builds
-                .where((b) => b.talents_names.length == 7));
-        if (interestingPopularBuilds.length > 0) {
-          children.add(new ListView(
-            children: interestingPopularBuilds
-                .map((b) => _buildTalentRow(context, b))
-                .toList(),
-          ));
-        }
-      }
-      return new TabBarView(
-        key: new Key(hero.name + '_talent_rows'),
-        children: children,
-      );
-    }
-    return new Container();
-  }
-
-  Widget _buildTalentRow(BuildContext context, BuildStatistics build) {
-    return new Padding(
-        key: new Key('${hero.name}_talent_row_${build.hashCode}'),
-        padding: new EdgeInsets.all(4.0),
-        child: new Column(
-          children: [
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: new EdgeInsets.symmetric(horizontal: 4.0),
-                  child: new Text(
-                      '${(build.win_rate * 100).toStringAsFixed(2)} Win %'),
-                ),
-                new Padding(
-                  padding: new EdgeInsets.symmetric(horizontal: 4.0),
-                  child: new Text('${build.total_games_played} Games Played'),
-                ),
-                new Padding(
-                    padding: new EdgeInsets.symmetric(horizontal: 4.0),
-                    child: new IconButton(
-                        icon: new Icon(Icons.play_circle_filled),
-                        onPressed: () {
-                          Navigator.of(context).push(new PageRouteBuilder(
-                                pageBuilder: (context, a1, a2) =>
-                                    new BuildSwiper(
-                                      hero,
-                                      build,
-                                      key: new Key(
-                                          '${hero.name}_${build.hashCode}_build_swiper'),
-                                    ),
-                              ));
-                        }))
-              ],
-            ),
-            new Container(
-              height: 4.0,
-            ),
-            new Padding(
-              padding: new EdgeInsets.symmetric(horizontal: 8.0),
-              child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: build.talents_names
-                      .map((a) => _buildTalent(context, a))
-                      .toList()),
-            ),
-          ],
-        ));
   }
 
   void showTalentBottomSheet(BuildContext context, Talent talent) {
@@ -383,52 +252,6 @@ class HeroDetail extends StatelessWidget {
                 )),
           );
         });
-  }
-
-  Widget _buildTalent(BuildContext context, String talentName) {
-    Talent talent = hero.talents
-        .firstWhere((t) => t.talent_tree_id == talentName, orElse: null);
-    if (talent == null) {
-      return new Icon(Icons.account_circle);
-    }
-    return new Expanded(
-      child: new GestureDetector(
-        onTap: () => showTalentBottomSheet(context, talent),
-        child: talent.have_asset
-            ? new Image.asset('assets/images/talents/${talent.icon_file_name}')
-            : new Image(
-                image: new CachedNetworkImageProvider(
-                    'https://s3.eu-west-1.amazonaws.com/data.heroescompanion.com/images/talents/${talent.icon_file_name}')),
-      ),
-    );
-  }
-
-  Widget _buildCardTalent(BuildContext context, String talentName) {
-    Talent talent =
-        hero.talents.firstWhere((t) => t.talent_tree_id == talentName);
-    // return new Flexible(
-    // fit: FlexFit.tight,
-    return new GestureDetector(
-        onTap: () => showTalentBottomSheet(context, talent),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            talent.have_asset
-                ? new Image.asset(
-                    'assets/images/talents/${talent.icon_file_name}')
-                : new Image(
-                    image: new CachedNetworkImageProvider(
-                        'https://s3.eu-west-1.amazonaws.com/data.heroescompanion.com/images/talents/${talent.icon_file_name}')),
-            new Container(
-              height: 4.0,
-            ),
-            new Text(
-              talent.name,
-              maxLines: 1,
-            )
-          ],
-          // ),
-        ));
   }
 
   List<Widget> _buildTabs(BuildContext context) {
@@ -469,14 +292,7 @@ class HeroDetail extends StatelessWidget {
               key: new Key('${hero.name}_previous_build_prompt'),
             ),
             _buildPhoneTitleRow(context),
-            new TabBar(
-              key: new Key('tab_bar_${tabs.hashCode}'),
-              tabs: tabs,
-              labelColor: Theme.of(context).textTheme.body2.color,
-            ),
-            new Expanded(
-              child: _buildTalentRows(context),
-            ),
+            new Expanded(child: _buildTalentCards(context))
           ],
         ));
   }
