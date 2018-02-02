cf157e9ca1257e8399d56da13aec9ae5bcee94f2
Arran France <arranf@gmail.com>
2018-02-02 20:48:25 +0000

    Add previous, next, and go to first buttons for build swiper

--- app/lib/view/common/build_swiper.dart
+++ app/lib/view/common/build_swiper.dart
@@ -58,20 +58,70 @@ class _BuildSwiperState extends State<BuildSwiper>
     super.dispose();
   }
 
+  void _nextPage(int delta) {
+    final int newIndex = _tabController.index + delta;
+    if (newIndex < 0 || newIndex >= _tabController.length)
+      return;
+    _tabController.animateTo(newIndex);
+  }
+
+  void _firstPage() {
+    _tabController.animateTo(0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Container(
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+    return new Card(
       key: new Key('${widget.hero.name}_swiper'),
       color: Theme.of(context).primaryColor,
-      child: new TabBarView(
-        key: new Key('${widget.hero.name}_swiper_tab_bar_view'),
-        controller: _tabController,
-        children: widget.buildWinRates.talents_names.map((String talentName) {
-          Talent talent = widget.hero.talents
-              .firstWhere((Talent t) => t.talent_tree_id == talentName);
-          return new TalentCard(talent, key: new Key(talentName));
-        }).toList(),
-      ),
+      child: new Column(
+        children: <Widget>[
+          new Padding(
+            padding: new EdgeInsets.only(top: statusBarHeight),
+            child: new Row(
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(Icons.first_page),
+                  color: Colors.white,
+                  onPressed: () => _firstPage(),
+                  tooltip: 'Go to first talent',
+                )
+              ],
+            ),
+          ),
+          new Flexible(
+            child: new TabBarView(
+              key: new Key('${widget.hero.name}_swiper_tab_bar_view'),
+              controller: _tabController,
+              children: widget.buildWinRates.talents_names.map((String talentName) {
+                Talent talent = widget.hero.talents
+                    .firstWhere((Talent t) => t.talent_tree_id == talentName);
+                return new TalentCard(talent, key: new Key(talentName));
+              }).toList(),
+            ),
+          ),
+         new Row(
+           mainAxisAlignment: MainAxisAlignment.spaceAround,
+           children: <Widget>[
+              new IconButton(
+                color: Colors.white,
+                disabledColor: Colors.grey,
+                icon: new Icon(Icons.arrow_back),
+                tooltip: 'Previous talent',
+                onPressed: () => _nextPage(-1),
+              ),
+              new TabPageSelector(controller: _tabController, color: Theme.of(context).primaryColor, selectedColor: Colors.white),
+              new IconButton(
+                color: Colors.white,
+                icon: new Icon(Icons.arrow_forward),
+                tooltip: 'Next talent',
+                onPressed: () => _nextPage(1),
+              ),
+           ],
+         )
+        ],
+      )
     );
   }
 }
