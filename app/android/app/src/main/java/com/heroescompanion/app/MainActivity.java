c9c9c14c3f9abbfb7566728ca3d03c73d199e6f8
Arran France <arranf@gmail.com>
2018-02-06 23:51:56 +0000

    Experiment with removing method channel

--- app/android/app/src/main/java/com/heroescompanion/app/MainActivity.java
+++ app/android/app/src/main/java/com/heroescompanion/app/MainActivity.java
@@ -1,19 +1,24 @@
 package com.heroescompanion.app;
 
-import android.os.Bundle;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
+
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.view.FlutterView;
+
+// import android.os.Bundle;
+// import android.content.ContextWrapper;
+// import android.content.Intent;
+// import android.content.IntentFilter;
+// import android.os.Build.VERSION;
+// import android.os.Build.VERSION_CODES;
+// import android.os.Bundle;
+// import io.flutter.app.FlutterActivity;
+// import io.flutter.plugins.GeneratedPluginRegistrant;
+// import io.flutter.plugin.common.MethodCall;
+// import io.flutter.plugin.common.MethodChannel;
+// import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+// import io.flutter.plugin.common.MethodChannel.Result;
+// import io.flutter.view.FlutterView;
 
 
 public class MainActivity extends FlutterActivity {
@@ -24,17 +29,17 @@ public class MainActivity extends FlutterActivity {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
     
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-      new MethodCallHandler() {
-        @Override
-        public void onMethodCall(MethodCall call, Result result) {
-          FlutterView view = getFlutterView();
-          if (call.method.equals("setScreenNoSleep")){
-            view.setKeepScreenOn(true);
-          } else if (call.method.equals("setScreenCanSleep")) {
-            view.setKeepScreenOn(false);
-          }
-        }
-    });
+    // new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+    //   new MethodCallHandler() {
+    //     @Override
+    //     public void onMethodCall(MethodCall call, Result result) {
+    //       FlutterView view = getFlutterView();
+    //       if (call.method.equals("setScreenNoSleep")){
+    //         view.setKeepScreenOn(true);
+    //       } else if (call.method.equals("setScreenCanSleep")) {
+    //         view.setKeepScreenOn(false);
+    //       }
+    //     }
+    // });
   }
 }
