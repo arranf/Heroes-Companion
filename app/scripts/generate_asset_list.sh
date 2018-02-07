2582f3574e2563b05a44090b33bcddff4c100f5c
Arran France <arranf@gmail.com>
2018-02-07 09:18:10 +0000

    Update bash scripts to be more resilient

--- app/scripts/generate_asset_list.sh
+++ app/scripts/generate_asset_list.sh
@@ -1,3 +1,11 @@
+# Check working directory and exit if bad
+DIR="$(basename $(pwd) )"
+if [ $DIR != 'scripts' ]; then
+    echo "In /$DIR"
+    echo "This must be run from inside the /scripts directory"
+    exit 1
+fi
+
 find ../assets/ -type f -name "*.png" > ls.txt
 # Replace the start of a line ../ with four spaces and a -
 sed -i -e 's/^..\//    - /' ls.txt 
