4fa79485eb8886e0b0aa69014a68b32cafb5dfc5
Arran France <arranf@gmail.com>
2018-04-11 14:42:51 +0100

    Fix another exception

--- heroes_companion_data/lib/src/api/DTO/rotation_data.dart
+++ heroes_companion_data/lib/src/api/DTO/rotation_data.dart
@@ -14,15 +14,18 @@ class RotationData {
     }
     DateTime rotationEnd = DateTime.parse(map['end'] as String);
     
-    List<Map<String, dynamic>> heroInfo = map['heroes'];
-    if (!heroInfo.every((m) => m['isFreeToPlay'] is bool)) {
+    List<dynamic> heroInfo = map['heroes'] as List<dynamic>;
+    if (!heroInfo.every((m) {
+        Map<dynamic, dynamic> map = m as Map<dynamic, dynamic>;
+        return map['isFreeToPlay'] is bool;
+      })) {
       throw new Exception('Rotation Data: Unexpected JSON format. Not every isFreeToPlay is bool');
     }
     
-    List<String> heroNames = heroInfo
+    List<dynamic> heroNames = heroInfo
         .where((hero) => hero['isFreeToPlay'] as bool)
         .map((hero) => hero['name'])
         .toList();
-    return new RotationData(rotationEnd, heroNames);
+    return new RotationData(rotationEnd, heroNames.cast<String>());
   }
 }
